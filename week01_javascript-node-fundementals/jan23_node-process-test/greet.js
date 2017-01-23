77ad60153bab1d66c28141cdef56142104f57a9d
martypdx <marty@martypdx.com>
2017-01-23 12:16:07 -0800

    initial passing tests

--- week01_javascript-node-fundementals/jan23_node-process-test/greet.js
+++ week01_javascript-node-fundementals/jan23_node-process-test/greet.js
@@ -1,5 +1,8 @@
-// entire line typed in as "command"
-// seperated by " "
-var name = process.argv[2];
+// entire line typed in as "command" seperated by " "
+
+var name = process.argv[2] || 'stranger';
+
+// same as testing after the fact:
+// if(!name) name = 'stranger';
 
 process.stdout.write(`hello ${name}`);
--- week01_javascript-node-fundementals/jan23_node-process-test/test/greet.test.js
+++ week01_javascript-node-fundementals/jan23_node-process-test/test/greet.test.js
@@ -2,14 +2,21 @@ var child_process = require('child_process');
 var assert = require('assert');
 
 it('greets by name', function() {
-    var actual = child_process.execFileSync(
+    var output = child_process.execFileSync(
         'node',
         ['greet.js', 'marty'],
         { encoding: 'utf8' }
     );
+    
+    assert.equal(output, 'hello marty');
+});
 
-    var expected = 'hello marty';
+it('uses "stranger" as a default when no name provided', function() {
+    var output = child_process.execFileSync(
+        'node',
+        ['greet.js'],
+        { encoding: 'utf8' }
+    );
 
-    assert.equal(actual, expected);
-
+    assert.equal(output, 'hello stranger');
 });
