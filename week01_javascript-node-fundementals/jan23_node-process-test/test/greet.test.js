f9049f21ae1e95f2e37fd843bd06273846408c40
martypdx <marty@martypdx.com>
2017-01-23 12:17:22 -0800

    extract method for running executable

--- week01_javascript-node-fundementals/jan23_node-process-test/test/greet.test.js
+++ week01_javascript-node-fundementals/jan23_node-process-test/test/greet.test.js
@@ -1,22 +1,20 @@
 var child_process = require('child_process');
 var assert = require('assert');
 
-it('greets by name', function() {
-    var output = child_process.execFileSync(
+function run(args) {
+    return child_process.execFileSync(
         'node',
-        ['greet.js', 'marty'],
+        args,
         { encoding: 'utf8' }
     );
-    
+}
+
+it('greets by name', function() {
+    var output = run(['greet.js', 'marty']);
     assert.equal(output, 'hello marty');
 });
 
 it('uses "stranger" as a default when no name provided', function() {
-    var output = child_process.execFileSync(
-        'node',
-        ['greet.js'],
-        { encoding: 'utf8' }
-    );
-
+    var output = run(['greet.js']);
     assert.equal(output, 'hello stranger');
 });
