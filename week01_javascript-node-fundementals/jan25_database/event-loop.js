fcc3a3560fe4d8d01295369bc750321dfcfa4d5a
martypdx <marty@martypdx.com>
2017-01-25 15:06:49 -0800

    TDD async demo

--- week01_javascript-node-fundementals/jan25_database/event-loop.js
+++ week01_javascript-node-fundementals/jan25_database/event-loop.js
@@ -1,24 +1,24 @@
 const request = require('request');
 const fs = require('fs');
 
-request('http://www.google.com', (err, res, body) => {
+request('http://www.google.com', function(err, res, body) {
     console.log('google:', body.slice(0, 10));
 });
 
-fs.readFile('package.json', 'utf-8', (err, file) => {
+fs.readFile('package.json', 'utf-8', function(err, file) {
     console.log('file:', file.slice(0, 10))
 });
 
-setTimeout(() => {
+setTimeout(function() {
     console.log('10ms later...');
 }, 10);
 
-setTimeout(() => {
+setTimeout(function() {
     console.log('5s later...');
 }, 5000);
 
-console.log('I guess node is done for now...');
+console.log('I guess this execution is done for now...');
 
-console.log(process._getActiveRequests());
-console.log(process._getActiveHandles());
+// console.log(process._getActiveRequests());
+// console.log(process._getActiveHandles());
 
