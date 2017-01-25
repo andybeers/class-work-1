563e5778bd238ef7869719eb0976c66175f6294d
martypdx <marty@martypdx.com>
2017-01-24 17:05:52 -0800

    additional changes

--- week01_javascript-node-fundementals/jan24_build-automation-dependency-management/cli.js
+++ week01_javascript-node-fundementals/jan24_build-automation-dependency-management/cli.js
@@ -1,4 +1,8 @@
 const addOne = require('./lib/add-one');
+const figlet = require('figlet');
 
 const added = addOne(+process.argv[2]);
-process.stdout.write(`${added}`);
+
+const display = figlet.textSync(`The answer is ${added}`);
+
+process.stdout.write(display);
--- week01_javascript-node-fundementals/jan24_build-automation-dependency-management/test/add-one.e2e.js
+++ week01_javascript-node-fundementals/jan24_build-automation-dependency-management/test/add-one.e2e.js
@@ -9,9 +9,16 @@ function run(args) {
     );
 }
 
+const figlet = `  _____ _                                                _       _  _   
+ |_   _| |__   ___    __ _ _ __  _____      _____ _ __  (_)___  | || |  
+   | | | '_ \ / _ \  / _\` | '_ \/ __\ \ /\ / / _ \ '__| | / __| | || |_ 
+   | | | | | |  __/ | (_| | | | \__ \\ V  V /  __/ |    | \__ \ |__   _|
+   |_| |_| |_|\___|  \__,_|_| |_|___/ \_/\_/ \___|_|    |_|___/    |_|  
+                                                                        `;
+
 describe('add one cli', function() {
     it('runs adds one', function() {
         var output = run(['cli.js', '3']);
-        assert.equal(output, '4');
+        assert.equal(output, figlet);
     });
 });
