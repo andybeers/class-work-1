2efe0ec2b9327057085accb459e6b3ddef56a3e6
martypdx <marty@martypdx.com>
2017-02-02 13:56:51 -0800

    using fs-promise

--- week02_servers/feb02-promises/basic-promises.js
+++ week02_servers/feb02-promises/basic-promises.js
@@ -1,28 +1,8 @@
-const fs = require('fs');
+const fs = require('fs-promise');
 
 function readFile(filename, cb) {
-
-    // create the new Promise and return it.
-    return new Promise((resolve, reject) => {
-        // resolve is the function we should use to return the value
-        // reject is the function we should use, to return an ERROR
-
-        console.log('about to read file');
-
-        // put the code to execute here
-        fs.readFile(filename, (err, data) => {
-            if(err) reject(err);
-            else resolve(data);
-        });
-
-    });
-
+    return fs.readFile(filename);
 }
 
-const promise = readFile('./bar.txt');
-
-// success handler :)
-promise.then(data => console.log(data));
-promise.then(data => console.log(data));
-// only an error handler
-promise.then(null, err => console.log('ERROR!', err));
+readFile('./foo.txt')
+    .then(data => console.log(data));
