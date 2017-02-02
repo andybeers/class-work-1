8762e705cd9358ad6d4db4d136040f7506410c92
martypdx <marty@martypdx.com>
2017-02-02 14:23:12 -0800

    basic promise chaining

--- week02_servers/feb02-promises/basic-promises.js
+++ week02_servers/feb02-promises/basic-promises.js
@@ -1,8 +1,21 @@
 const fs = require('fs-promise');
 
-function readFile(filename, cb) {
-    return fs.readFile(filename);
-}
+// function readFile(filename, cb) {
+//     return fs.readFile(filename);
+// }
 
-readFile('./foo.txt')
-    .then(data => console.log(data));
+// readFile('./foo.txt')
+//     .then(data => console.log(data));
+
+// const filename = 'test.txt';
+
+fs.writeFile(filename, 'This is the file')
+    .then(() => {
+        return fs.readFile(filename, { encoding: 'utf8' })
+            .then(contents => contents.toUpperCase());
+    })
+    .then(data => {
+        console.log('second chained then', data);
+    });
+
+// Promise.resolve(12).then(value => console.log(value));
