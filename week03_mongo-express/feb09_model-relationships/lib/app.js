9a27f540c345b32d1560d03dad2cd48d2792939f
martypdx <marty@martypdx.com>
2017-02-09 16:40:52 -0800

    show adding child records to parent

--- week03_mongo-express/feb09_model-relationships/lib/app.js
+++ week03_mongo-express/feb09_model-relationships/lib/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const pets = require('./routes/pets');
 const petTypes = require('./routes/pet-types');
+const stores = require('./routes/stores');
 
 const logger = require('logger')();
 app.use(logger);
@@ -14,6 +15,7 @@ const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
+app.use('/stores', stores);
 app.use('/pets', pets);
 app.use('/pet-types', petTypes);
 
