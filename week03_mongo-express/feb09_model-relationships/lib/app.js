bbc823ef4f0ac4642fbdbeee5cfd2cbeb97fb9d4
martypdx <marty@martypdx.com>
2017-02-09 16:15:41 -0800

    mongoose model relationships

---
+++ week03_mongo-express/feb09_model-relationships/lib/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const pets = require('./routes/pets');
+const petTypes = require('./routes/pet-types');
+
+const logger = require('logger')();
+app.use(logger);
+
+// // logging:
+// const morgan = require('morgan');
+// app.use(morgan('dev'));
+
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
+app.use('/pets', pets);
+app.use('/pet-types', petTypes);
+
+module.exports = app;
