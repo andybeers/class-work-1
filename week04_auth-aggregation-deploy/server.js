fb25ec86ebb6f8f1ae9d0bd9868b7e224d53ef09
martypdx <marty@martypdx.com>
2017-02-13 14:22:21 -0800

    node server runs

---
+++ week04_auth-aggregation-deploy/server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+const app = require('express')();
+
+const server = http.createServer(app);
+const port = 3000 || process.env.PORT;
+
+server.listen(port, () => {
+    console.log('server running', server.address());
+});
