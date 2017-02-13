822650ec35380643f39e6a66d62f512941e8bf44
martypdx <marty@martypdx.com>
2017-02-13 15:09:38 -0800

    get actor by id

--- week04_auth-aggregation-deploy/lib/routes/actors.js
+++ week04_auth-aggregation-deploy/lib/routes/actors.js
@@ -7,4 +7,11 @@ module.exports = router
             .lean()
             .then(actors => res.send(actors))
             .catch(next);
+    })
+    .get('/:id', (req, res, next) => {
+        Actor.findById(req.params.id)
+            // TODO: check !actor and send 404
+            .then(actor => res.send(actor))
+            .catch(next);
+
     });
