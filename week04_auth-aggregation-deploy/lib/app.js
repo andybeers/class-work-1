bb81b300570fb3446f791f0ad9acf70098af58e6
martypdx <marty@martypdx.com>
2017-02-13 14:34:58 -0800

    add in actors router

--- week04_auth-aggregation-deploy/lib/app.js
+++ week04_auth-aggregation-deploy/lib/app.js
@@ -1,7 +1,10 @@
 const app = require('express')();
 const morgan = require('morgan')('dev');
+const actors = require('./routes/actors');
 
 app.use(morgan);
+
+app.use('/actors', actors);
 
 app.get('/status', (req, res) => {
     res.send('OK');
