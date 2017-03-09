71da5519d8616da30b883637127cde4cc8be804a
Dan Schuman <danschuman@gmail.com>
2017-03-09 12:10:05 -0800

    Init ChatApp

---
+++ week07_front-end-pt2/mar09_chat_app/chat-app/src/components/ChatApp.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component, PropTypes } from 'react';
+
+export default class ChatApp extends Component {
+  static propTypes = {
+  }
+
+  render() {
+    return (
+      <div className='chat-app'>
+        I am a full-height container.
+      </div>
+    );
+  }
+}
