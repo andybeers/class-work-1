6780ec77f5d4e23228fca75d166b4aa7f3c4d828
Dan Schuman <danschuman@gmail.com>
2017-03-22 13:03:26 -0700

    Init new blank project

---
+++ week09_final-week/mar22_various_components/animations/src/components/App/App.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line no-unused-vars
+import React, { Component, PropTypes } from 'react';
+
+require('./App.css');
+
+export default class App extends Component {
+  static propTypes = {
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        Hello!
+      </div>
+    );
+  }
+}
