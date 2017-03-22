63b99829950a76ac98fe685bbd1abb1ef2dc14b9
Dan Schuman <danschuman@gmail.com>
2017-03-22 14:09:32 -0700

    Add some utils and a list of images

--- week09_final-week/mar22_various_components/animations/src/components/App/App.js
+++ week09_final-week/mar22_various_components/animations/src/components/App/App.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
 
 require('./App.css');
 
@@ -10,7 +11,43 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        Hello!
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {items: _.times(200)};
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    const newItems = this.state.items.concat([
+      prompt('Enter some text')
+    ]);
+    this.setState({items: newItems});
+  }
+
+  handleRemove(i) {
+    let newItems = this.state.items.slice();
+    newItems.splice(i, 1);
+    this.setState({items: newItems});
+  }
+
+  render() {
+    const items = this.state.items.map((item, i) => (
+      <div className='example' key={item} onClick={() => this.handleRemove(i)}>
+        <img src={`http://placehold.it/300?text=${Math.random(10000)}`} />
+      </div>
+    ));
+
+    return (
+      <div>
+        <button onClick={this.handleAdd}>Add Item</button>
+        {items}
       </div>
     );
   }
